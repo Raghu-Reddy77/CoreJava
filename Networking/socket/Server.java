17b339d9ef85cfa92a816270233bc3f445182d65
Raghu-Reddy77 <raghu.chavva2023@gmail.com>
2023-08-23 22:06:29 +0530

    socket programming

--- Networking/socket/Server.java
+++ Networking/socket/Server.java
@@ -1,5 +1,41 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        // Create a server socket on port 9999
+        ServerSocket ss = new ServerSocket(9999);
+
+        // Wait for a client to connect and accept the connection
+        Socket clientSocket = ss.accept();
         
+        // Get input stream to receive data from the client
+        InputStream is = clientSocket.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        
+        // Read the client's message
+        String clientMessage = br.readLine();
+        System.out.println("Client message: " + clientMessage);
+        
+        // Get output stream to send data back to the client
+        OutputStream os = clientSocket.getOutputStream();
+        PrintStream ps = new PrintStream(os);
+        
+        // Respond to the client's message
+        String response = "Message received: " + clientMessage;
+        ps.println(response);
+
+        // Close the client socket and server socket
+        clientSocket.close();
+        ss.close();
     }
 }
--- Networking/socket/client.java
+++ Networking/socket/client.java
@@ -1,8 +1,38 @@
-/**
- * client
- */
-public class client {
-    public static void main(String[] args) {
-       
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) { 
+        try {
+            // Create a socket connection to the server at localhost and port 9999
+            Socket socket = new Socket("localhost", 9999);
+            
+            // Get the output stream to send data to the server
+            OutputStream os = socket.getOutputStream();
+            PrintStream ps = new PrintStream(os);
+            
+            // Send a message to the server
+            String data = "Hello, server!";
+            ps.println(data);
+            
+            // Get the input stream to receive data from the server
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String response = br.readLine();
+            
+            // Print the response received from the server
+            System.out.println("Server response: " + response);
+            
+            // Close the socket connection
+            socket.close();
+        } catch (IOException e) {
+            // Handle exceptions that may occur during socket communication
+            e.printStackTrace();
+        }
     }
 }
